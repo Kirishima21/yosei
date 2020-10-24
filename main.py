8be6c81510ceec0f474992f6cacea8d93b169a8b
霧島ひなた <mastodon@mail.kirishima.cloud>
2020-10-24 21:15:45 +0900

    add medicines dic
    薬剤名を重複なく登録できるように機能を追加した。

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import PySimpleGUI as sg
+import pandas as pd
 
 # 各種設定ファイルのロード
 from layout.layout import layout_master
-
 
 
 def simple_gui():
@@ -70,7 +70,18 @@ def simple_gui():
 
         # 医薬品名の登録をしたときの処理
         if event == "add_medicines_name":
-            sg.popup("登録が完了しました。")
+            df = pd.read_excel('data.xlsx', sheet_name=None)
+            bool = not any(df["Sheet2"]["name"].str.contains(str(data)))
+            if bool:
+                df_add = pd.DataFrame([data], columns=['name'])
+                df1 = df["Sheet1"]
+                df2 = df["Sheet2"].append(df_add)
+                with pd.ExcelWriter('data.xlsx') as writer:
+                    df1.to_excel(writer, sheet_name='Sheet1', index = False)
+                    df2.to_excel(writer, sheet_name='Sheet2', index=False, header="name")
+                sg.popup("登録が完了しました。")
+            else:
+                sg.popup("登録済みの医薬品名です")
 
 
     # 前ページ共通の処理
