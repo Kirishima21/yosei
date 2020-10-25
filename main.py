c3d9cdd5028a752a60ccedfb71b751f472eba503
霧島ひなた <mastodon@mail.kirishima.cloud>
2020-10-25 22:05:18 +0900

    add register pre-made
    予製を登録する機能を追加
    ・重複登録を回避する機構が未実装

--- main.py
+++ main.py
@@ -83,6 +83,42 @@ def simple_gui():
             else:
                 sg.popup("登録済みの医薬品名です")
 
+    # 予製登録ページ関連の処理
+
+        # 予製を登録するボタンを押したときの処理
+        if event == "add_yosei":
+            data = values
+            count = 0
+            index_count = 1
+            df = pd.read_excel('data.xlsx', sheet_name=None)
+            df1 = df["Sheet1"]
+            df2 = df["Sheet2"]
+
+            print("\n\n\n読み込まれたデータ")
+            print(df1)
+
+            df_add = pd.DataFrame({'患者名': [data["personal_name"]],
+                                   '来局予定日': [data["date"]],
+                                   '来局状態': ['未']},
+                                  index=['data'])
+
+            while count < 40:
+                medicine = {data[count]: data[count + 1]}
+                index_name = "薬剤" + str(index_count)
+                s = pd.DataFrame({index_name: str(medicine)}, index=['data'])
+                df_add = pd.concat([df_add, s], axis = 1)
+                index_count +=1
+                count += 2
+
+            df1 = df1.append(df_add)
+
+            with pd.ExcelWriter('data.xlsx') as writer:
+                df1.to_excel(writer, sheet_name='Sheet1', index = False, )
+                df2.to_excel(writer, sheet_name='Sheet2', index = False, header="name")
+
+            sg.popup("予製を登録しました")
+
+
 
     # 前ページ共通の処理
 
